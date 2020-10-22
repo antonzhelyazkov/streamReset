594b077d9cbed3e33fe63d0766e3d28d00409500
antonzhelyazkov <antonisimo@gmail.com>
2020-10-22 23:01:43 +0300

    init

--- main.py
+++ main.py
@@ -1,1 +1,72 @@
-print("hello")
+import getopt
+import json
+import logging
+import os
+import sys
+
+verbose = False
+config_file = "./config.json"
+log_dir = "."
+dry_run = False
+argv = sys.argv[1:]
+
+try:
+    opts, argv = getopt.getopt(argv, "c:vy", ["config=", "verbose", "dry="])
+except getopt.GetoptError as err:
+    print(err)
+    opts = []
+
+for opt, arg in opts:
+    if opt in ['-c', '--config']:
+        config_file = arg
+    if opt in ['-v', '--verbose']:
+        verbose = True
+    if opt in ['-y', '--dry']:
+        dry_run = True
+    else:
+        print("HELP")
+
+
+def print_log(debug, message):
+    try:
+        current_log_dir = config_data['log_dir']
+    except Exception:
+        current_log_dir = log_dir
+
+    if os.path.isdir(current_log_dir):
+        log_file_name = os.path.basename(sys.argv[0]).split(".")
+        script_log = current_log_dir + "/" + log_file_name[0] + ".log"
+        logging.basicConfig(filename=script_log, level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+        logging.info(message)
+        if debug is True:
+            print(message)
+
+    else:
+        print(f"log directory does not exist {current_log_dir}")
+        exit(1)
+
+
+try:
+    config_open = open(config_file, encoding='utf-8')
+except Exception as e:
+    print_log(verbose, e)
+    exit(1)
+else:
+    config_data = json.load(config_open)
+
+pid_file_path = config_data['pid_file_path']
+file_name = os.path.basename(sys.argv[0]).split(".")
+pid_file = pid_file_path.rstrip('/') + "/" + file_name[0] + ".pid"
+
+isPID = os.path.isfile(pid_file)
+if isPID:
+    print("PID file exists " + pid_file)
+    sys.exit()
+else:
+    f = open(pid_file, "w")
+    f.write(str(os.getpid()))
+    f.close()
+
+
+
+# os.remove(pid_file)
