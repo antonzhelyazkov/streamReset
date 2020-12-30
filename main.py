d541a189d35ac467766eb92ef9396315395588bc
aantonov <antonisimo@gmail.com>
2020-12-30 13:05:12 +0200

    test1

--- main.py
+++ main.py
@@ -113,6 +113,7 @@ arr_couples = stream_couples(config_data['apps'])
 for c_app, c_stream in arr_couples:
     m3u8_url = m3u8_stream(c_app, c_stream)
     try:
+        print("qweqweqweqweqweqwe")
         status = requests.get(m3u8_url).status_code
     except requests.HTTPError as e:
         print_log(verbose, f"ERROR http {e}")
